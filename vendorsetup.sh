c1f299b1c4922df13c3ac3d9315cb5bacddaba75
Kazakov Stepan <kazakov1999stepan1948.12@gmail.com>
2016-09-24 15:45:04 +0400

    use ccache

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 if [ -e /media/ksrt12/fb1f99a4-69a9-469e-94b5-ce9b2e8636be/ksrt12/UBERTC/out/arm-linux-androideabi-4.9-cortex-a5/bin/arm-linux-androideabi-gcc ]; then export TARGET_KERNEL_CROSS_COMPILE_PREFIX=/media/ksrt12/fb1f99a4-69a9-469e-94b5-ce9b2e8636be/ksrt12/UBERTC/out/arm-linux-androideabi-4.9-cortex-a5/bin/arm-linux-androideabi- ;fi
+export USE_CCACHE=1
 add_lunch_combo cm_u8950-userdebug
 add_lunch_combo cm_u8950-eng
